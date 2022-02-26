3e3f143e2036c4b884e2d8006f64dda7ee35e341
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-02-26 11:32:31 +0330

    add volume button and change buttons visibility

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const playBtn = document.querySelector('.gg-play-button')
 const pauseBtn = document.querySelector('.gg-play-pause')
 const fullScreenBtn = document.querySelector('.gg-assign')
 const settings = document.querySelector('.gg-more')
+const volume = document.querySelector('.gg-volume')
+const buttons = document.querySelector('.buttons')
+const videoContainer = document.querySelector('.video_container')
 
 const playVideo = () => {
   videoElem.play()
@@ -20,7 +23,37 @@ const openSettings = () => {
   console.log("settings")
 }
 
+const toggleVolume = () => {
+  if (videoElem.volume == 0) {
+    videoElem.volume = 1
+  } else {
+    videoElem.volume = 0
+  }
+}
+
+const showButtons = () => {
+  buttons.style.display = "flex"
+}
+
+const hideButtons = () => {
+  buttons.style.display = "none"
+}
+
+const togglePlaying = () => {
+  if (videoElem.paused) {
+    videoElem.play()
+  } else {
+    videoElem.pause()
+  }
+}
+
+buttons.style.display = "none"
+
 playBtn.addEventListener('click', playVideo)
 pauseBtn.addEventListener('click', pauseVideo)
 fullScreenBtn.addEventListener('click', expandScreen)
 settings.addEventListener('click', openSettings)
+volume.addEventListener('click', toggleVolume)
+videoContainer.addEventListener('mouseover', showButtons)
+videoContainer.addEventListener('mouseout', hideButtons)
+videoElem.addEventListener('click', togglePlaying)
