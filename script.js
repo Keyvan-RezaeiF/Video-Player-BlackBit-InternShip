6f62410921e065337e0841f2a89d6fe48d3dcd9f
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-02-26 11:00:41 +0330

    add fullscreen & settings buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const videoElem = document.querySelector('video')
 const playBtn = document.querySelector('.gg-play-button')
 const pauseBtn = document.querySelector('.gg-play-pause')
+const fullScreenBtn = document.querySelector('.gg-assign')
+const settings = document.querySelector('.gg-more')
 
 const playVideo = () => {
   videoElem.play()
@@ -10,5 +12,15 @@ const pauseVideo = () => {
   videoElem.pause()
 }
 
+const expandScreen = () => {
+  videoElem.requestFullscreen()
+}
+
+const openSettings = () => {
+  console.log("settings")
+}
+
 playBtn.addEventListener('click', playVideo)
 pauseBtn.addEventListener('click', pauseVideo)
+fullScreenBtn.addEventListener('click', expandScreen)
+settings.addEventListener('click', openSettings)
